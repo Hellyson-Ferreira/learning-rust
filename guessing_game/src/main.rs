09271718842dcd8a86ef7618d2f493a758495aaa
Hellyson-Ferreira <hellysonferreira.hf1@gmail.com>
2022-05-08 20:24:14 -0300

    guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,4 @@
 extern crate rand; // adicionando dependência externa
-
-
-use core::num;
 use std::io::stdin; // Importando função de um módulo
 use std::cmp::Ordering;
 use rand::Rng;
@@ -12,33 +9,29 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    // println!("O numero secreto é {}", secret_number);
-
-
-    println!("Digite o seu palpite ou 0 pra sair .");
-
-    
+    // println!("O numero secreto é {}", secret_number);    
 
     loop{
+
+        println!("Digite o seu palpite [0 pra sair.]");
 
         let mut guess = String::new();// Em rust por padrão todas a variáveis são imutáveis, o `mut` serve pra explicitar que um variável vai mudar de valor 
 
         stdin()
-        .read_line(&mut guess)//referências são imutáveis por padrão. Por isso, precisamos escrever &mut palpite, em vez de apenas &palpite
+        .read_line(&mut guess)//referências são imutáveis por padrão. Por isso, precisamos escrever &mut guess, em vez de apenas &guess
         .expect("Falha ao ler entrada");
     
         println!("Você disse: {guess}");
     
         let guess: u32 = match guess.trim().parse(){
+                Err(_) => continue,
                 Ok(num) =>{ 
-
                     match num{
                         0 => break,
                         num => num
                     }
-
                 },
-                Err(_) => continue
+                
         };
     
     
@@ -52,11 +45,4 @@ fn main() {
         }
 
     }
-
-    
-
-
-
-
-
 }
